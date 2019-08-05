34897431967f7fa6fc2f1bcc1cc10bfb87dd270e
Leo Borquez <ele.dev@protonmail.com>
2019-08-04 22:10:39 -0400

    react ui

--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "./assets/css/nucleo-icons.css";
+import "./assets/scss/blk-design-system-react.scss?v=1.0.0";
+import "./assets/demo/demo.css";
+
+import Index from "./views/Index.jsx";
+import LandingPage from "./views/examples/LandingPage.jsx";
+import RegisterPage from "./views/examples/RegisterPage.jsx";
+import ProfilePage from "./views/examples/ProfilePage.jsx";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/components" render={props => <Index {...props} />} />
+            <Route
+                path="/landing-page"
+                render={props => <LandingPage {...props} />}
+            />
+            <Route
+                path="/register-page"
+                render={props => <RegisterPage {...props} />}
+            />
+            <Route
+                path="/profile-page"
+                render={props => <ProfilePage {...props} />}
+            />
+            <Redirect from="/" to="/components" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
